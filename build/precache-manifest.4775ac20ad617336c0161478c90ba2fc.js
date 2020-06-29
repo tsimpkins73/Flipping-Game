bb8837fa5c4755a548b8926e07efa837db4a88a9
Travis Simpkins <t73designs@yahoo.com>
2020-06-29 11:17:03 -0400

    Fully Functional

---
+++ build/precache-manifest.4775ac20ad617336c0161478c90ba2fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdafb63c368470fd0caad8a8cf73e56a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fa5d407fb7d5a712c8",
+    "url": "/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "fe6b658886ffee6caec8",
+    "url": "/static/css/main.1ede1b47.chunk.css"
+  },
+  {
+    "revision": "89fa5d407fb7d5a712c8",
+    "url": "/static/js/2.b93eccd3.chunk.js"
+  },
+  {
+    "revision": "fe6b658886ffee6caec8",
+    "url": "/static/js/main.847bb7ce.chunk.js"
+  },
+  {
+    "revision": "03bda1db4b940ec66c4f",
+    "url": "/static/js/runtime-main.f5f1b176.js"
+  }
+]);

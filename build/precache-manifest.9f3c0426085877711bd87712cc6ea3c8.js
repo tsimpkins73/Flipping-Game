d31d14cfa4b23477a2abd051e8718631fe66f494
Travis Simpkins <t73designs@yahoo.com>
2020-06-29 12:02:34 -0400

    Fix Set Prices

---
+++ build/precache-manifest.9f3c0426085877711bd87712cc6ea3c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecb233ad09542b4e5fb90e89138c243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edd5df7c0fd33959d3d",
+    "url": "/static/css/2.b8d7bdcb.chunk.css"
+  },
+  {
+    "revision": "714ad410e5ec9c3dc43a",
+    "url": "/static/css/main.1ede1b47.chunk.css"
+  },
+  {
+    "revision": "2edd5df7c0fd33959d3d",
+    "url": "/static/js/2.2ae66be3.chunk.js"
+  },
+  {
+    "revision": "714ad410e5ec9c3dc43a",
+    "url": "/static/js/main.b0b7cce5.chunk.js"
+  },
+  {
+    "revision": "b35e1c9b376079ca7b5b",
+    "url": "/static/js/runtime-main.bc888313.js"
+  }
+]);
